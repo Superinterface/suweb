ccdb1e929d9501c35c93d2e6cbf61533638ebb03
Superinterface <su_jia_nan@163.com>
2020-01-20 15:54:20 +0800

    提交 修复关于博客上传找不到对应博客类型配置数据库数据以及创建对应文件夹的bug. 已修改为 在新增/修改/删除"博客类型"数据字典下级数据字典的时候一起更新博客上传 表数据

--- src/main/java/com/sujianan/service/system/DataDictionaryService.java
+++ src/main/java/com/sujianan/service/system/DataDictionaryService.java
@@ -1,11 +1,13 @@
 package com.sujianan.service.system;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sujianan.bean.blog.BlogUploadUrl;
 import com.sujianan.bean.frame.layui.JsonForLayuiData;
 import com.sujianan.bean.frame.layui.JsonForLayuiRoot;
 import com.sujianan.bean.system.DataDictionary;
 import com.sujianan.bean.user.User;
 import com.sujianan.dao.blog.BlogMapper;
+import com.sujianan.dao.blog.BlogUploadUrlMapper;
 import com.sujianan.dao.system.DataDictionaryMapper;
 import com.sujianan.util.DefaultUtil;
 import com.sujianan.util.HttpResponse;
@@ -20,6 +22,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class DataDictionaryService {
@@ -27,6 +30,8 @@ public class DataDictionaryService {
 	DataDictionaryMapper datadictionarymapper;
 	@Autowired
 	BlogMapper blogMapper;
+	@Autowired
+	BlogUploadUrlMapper blogUploadUrlMapper;
 
 	public HttpResponse<Object> getDataByCode(String code) {
 		List<DataDictionary> ddlist = this.datadictionarymapper.selectByCode(code);
@@ -91,6 +96,7 @@ public class DataDictionaryService {
 		return new HttpResponse<Object>(RST.CODE_SUCCESS, RST.TEXT_SUCCESS, dd);
 	}
 
+	@Transactional
 	public HttpResponse<Object> addDictionary(HttpServletRequest request, DataDictionary dd) {
 		if (dd == null || "".equals(dd.getDataCode()) || "".equals(dd.getDataName()) || null == dd.getPid())
 			return new HttpResponse<Object>(RST.CODE_ERROR, RST.TEXT_PARAM_ERROR, null);
@@ -98,7 +104,7 @@ public class DataDictionaryService {
 		try {
 			User user = DefaultUtil.getUserForRequest(request);
 			DataDictionary pdd = datadictionarymapper.selectByPrimaryKey(dd.getPid());
-			if(null == pdd) return new HttpResponse<Object>(404, "错误的数据", null);
+			if(null == pdd) return new HttpResponse<Object>(RST.CODE_ERROR, RST.TEXT_PARAM_ERROR, null);
 			dd.setDataLevel((short) (pdd.getDataLevel() + 1));
 			dd.setCreateTime(new Date());
 			dd.setCreateUser(Integer.toString(user.getId()));
@@ -106,7 +112,9 @@ public class DataDictionaryService {
 			/* 针对博客数据字典进行特殊逻辑处理,因为上传博客需要上传至对应文件夹内,而对应文件夹是根据数据库的表来进行创建的,
 			故,当前端能选择到当前这个博客类型的时候,则数据库内必须已经有该类型的文件夹记录才足以进行博客上传功能.
 			判断是否是博客数据字典增改,如果是,则更新对应博客数据库表记录表*/
-			
+			if(findUperDataDictionary(dd.getDataCode())){
+				blogUploadUrlMapper.insert(new BlogUploadUrl(dd.getDataCode(), "/" + dd.getDataCode()));
+			}
 			returnFalg = true;
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -116,6 +124,7 @@ public class DataDictionaryService {
 				: new HttpResponse<Object>(RST.CODE_ERROR, RST.TEXT_ERROR, null);
 	}
 
+	@Transactional
 	public HttpResponse<Object> updateDictionary(HttpServletRequest request, DataDictionary dd) {
 
 		if (dd == null || "".equals(dd.getDataCode()) || "".equals(dd.getDataName()) || null == dd.getId())
@@ -132,6 +141,13 @@ public class DataDictionaryService {
 			dd.setUpdateTime(new Date());
 			dd.setUpdateUser(Integer.toString(user.getId()));
 			datadictionarymapper.updateByPrimaryKeySelective(dd);
+			// 修改博客类型的数据字典的时候,同时修改博客上传表数据
+			if (findUperDataDictionary(dd.getDataCode())) {
+				BlogUploadUrl buu = blogUploadUrlMapper.selectByDataCode1(oldData.getDataCode());
+				buu.setDataCode(dd.getDataCode());
+				buu.setDirectory("/" + dd.getDataCode());
+				blogUploadUrlMapper.updateByPrimaryKey(buu);
+			}
 			returnFalg = true;
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -141,6 +157,7 @@ public class DataDictionaryService {
 				: new HttpResponse<Object>(RST.CODE_ERROR, RST.TEXT_ERROR, null);
 	}
 
+	@Transactional
 	public HttpResponse<Object> deleteDictionaryById(Integer id) {
 		if (null == id || id == 0) // Integer 使用 == 进行判断时只对 -128~127 有效, 因为源码有缓存cache[], 否则使用eq判断
 			return new HttpResponse<Object>(RST.CODE_ERROR, RST.TEXT_PARAM_ERROR, null);
@@ -151,6 +168,11 @@ public class DataDictionaryService {
 			if (ddlist != null && ddlist.size() > 0) { // 有下级的数据字典不能删除
 				return new HttpResponse<Object>(RST.CODE_ERROR, RST.SYSTEM_DATA_DICTIONARY_HAVE_CHILDREN, null);
 			}
+			DataDictionary dd = datadictionarymapper.findDataById(id);
+			// 删除关于博客的数据字典需要一起删除博客上传类型的表数据
+			if(findUperDataDictionary(dd.getDataCode())) {
+				blogUploadUrlMapper.deleteByCode(dd.getDataCode());
+			}
 			row = datadictionarymapper.deleteByPrimaryKey(id);
 			returnFalg = row > 0 ? true : returnFalg;
 		} catch (Exception e) {
@@ -159,6 +181,19 @@ public class DataDictionaryService {
 
 		return returnFalg ? new HttpResponse<Object>(RST.CODE_SUCCESS, RST.TEXT_SUCCESS, null)
 				: new HttpResponse<Object>(RST.CODE_ERROR, RST.TEXT_ERROR, null);
+	}
+	
+	/**
+	 * 查找上级数据字典,查看是否是节点博客类型(blog_type)的子节点
+	 */
+	public boolean findUperDataDictionary(String code) {
+		List<DataDictionary> ddlist = datadictionarymapper.findUperDataDictionary(code);
+		for (DataDictionary dd : ddlist) {
+			if("blog_type".equals(dd.getDataCode())) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
